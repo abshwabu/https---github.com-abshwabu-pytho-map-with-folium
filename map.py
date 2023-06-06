e7cff1ec5ec89b33dc3a0918787b1f3590b6b78c
ashwabu <abdulazizredi90@gmail.com>
2023-06-06 23:01:32 +0300

    code

--- map.py
+++ map.py
@@ -2,6 +2,10 @@ import folium
 
 map = folium.Map(location=[8.9806, 38.7578],zoom_start=12,tiles = "Stamen Terrain")
 
-map.add_child(folium.Marker(location=[8.9806,38.7578],popup='AA',icon=folium.Icon(color='green')))
+fg = folium.FeatureGroup(name='My Map')
+
+fg.add_child(folium.Marker(location=[8.9806,38.7578],popup='AA',icon=folium.Icon(color='green')))
+
+map.add_child(fg)
 
 map.save('map.html')
