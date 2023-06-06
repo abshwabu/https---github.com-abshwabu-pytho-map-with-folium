5b0e9c599f00797520989da38006894cfbe8fa85
ashwabu <abdulazizredi90@gmail.com>
2023-06-06 22:57:56 +0300

    zoom

--- map.py
+++ map.py
@@ -1,6 +1,6 @@
 import folium
 
-map = folium.Map(location=[8.9806, 38.7578],zoom_start=2,tiles = "Stamen Terrain")
+map = folium.Map(location=[8.9806, 38.7578],zoom_start=12,tiles = "Stamen Terrain")
 
 map.add_child(folium.Marker(location=[8.9806,38.7578],popup='AA',icon=folium.Icon(color='green')))
 
