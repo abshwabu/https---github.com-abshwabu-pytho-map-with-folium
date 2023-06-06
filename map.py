88ee29a395769ac8952313ff8718f8ef1de21f34
ashwabu <abdulazizredi90@gmail.com>
2023-06-06 22:55:17 +0300

    marker added

--- map.py
+++ map.py
@@ -1,5 +1,7 @@
 import folium
 
-map = folium.Map(location=[8.9806, 38.7578],zoom_start=6,tiles = "Stamen Terrain")
+map = folium.Map(location=[8.9806, 38.7578],zoom_start=2,tiles = "Stamen Terrain")
+
+map.add_child(folium.Marker(location=[8.9806,38.7578],popup='AA',icon=folium.Icon(color='green')))
 
 map.save('map.html')
