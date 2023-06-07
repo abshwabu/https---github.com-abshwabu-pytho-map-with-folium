22cd41e7e64094d4b4644a927f7feb82e32dee35
ashwabu <abdulazizredi90@gmail.com>
2023-06-07 05:18:31 +0300

    name added

--- map.py
+++ map.py
@@ -4,12 +4,13 @@ import pandas
 data = pandas.read_csv('Volcanoes.txt')
 lat = list(data['LAT'])
 lon = list(data['LON'])
+names = list(data['NAME'])
 
-map = folium.Map(location=[8.9806, 38.7578],zoom_start=12,tiles = "Stamen Terrain")
+map = folium.Map(location=[48.7767982, -121.810997],zoom_start=12,tiles = "Stamen Terrain")
 
 fg = folium.FeatureGroup(name='My Map')
-for lt,ln in zip(lat,lon):
-    fg.add_child(folium.Marker(location=[lt,ln],popup='AA',icon=folium.Icon(color='green')))
+for lt,ln,name in zip(lat,lon,names):
+    fg.add_child(folium.Marker(location=[lt,ln],popup=name,icon=folium.Icon(color='green')))
 
 map.add_child(fg)
 
