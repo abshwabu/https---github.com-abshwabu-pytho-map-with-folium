48c2883c3a5743fcea7b067359195df3cb68c5c2
ashwabu <abdulazizredi90@gmail.com>
2023-06-07 16:41:15 +0300

    color zoom hight added

--- map.py
+++ map.py
@@ -5,12 +5,22 @@ data = pandas.read_csv('Volcanoes.txt')
 lat = list(data['LAT'])
 lon = list(data['LON'])
 names = list(data['NAME'])
+elv = list(data['ELEV'])
 
-map = folium.Map(location=[48.7767982, -121.810997],zoom_start=12,tiles = "Stamen Terrain")
+def color(elve):
+    if elve < 1000:
+        return 'green'
+    elif 1000 <= elve <3000:
+        return 'orange'
+    else:
+        return 'red' 
+
+
+map = folium.Map(location=[48.7767982, -121.810997],zoom_start=6,tiles = "Stamen Terrain")
 
 fg = folium.FeatureGroup(name='My Map')
-for lt,ln,name in zip(lat,lon,names):
-    fg.add_child(folium.Marker(location=[lt,ln],popup=name,icon=folium.Icon(color='green')))
+for lt,ln,name,el in zip(lat,lon,names,elv):
+    fg.add_child(folium.Marker(location=[lt,ln],popup=name +'\n'+str(el)+'m',icon=folium.Icon(color=color(el))))
 
 map.add_child(fg)
 
